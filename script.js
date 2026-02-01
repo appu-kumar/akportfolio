75f9350692d3b1eb08608e720be5f22917d58892
Appu Kumar <appu6688kumar@gmail.com>
2026-02-01 19:08:45 +0530

    add correct resume's url

--- script.js
+++ script.js
@@ -10,12 +10,12 @@ function downloadAndViewResume(e) {
     e.preventDefault();
     
     // Open for viewing in normal size
-    const viewUrl = 'https://drive.google.com/file/d/1L-o-Jf7KERVEkXV1z8jr_aN0JbBjHyFh/view?usp=sharing';
+    const viewUrl = 'https://drive.google.com/file/d/1n_uUXej_Tsrsf62a9_GX9Rm2gqgv6UK5/view?usp=sharing';
     window.open(viewUrl, '_blank');
     
     // For direct download in new tab
     // const downloadUrl = 'https://drive.google.com/uc?id=1In0TZxqQ4xc-GpkI7B4Gzmh_YHNll9ie&export=download&confirm=t';
-    const downloadUrl = "https://drive.google.com/uc?export=download&id=1L-o-Jf7KERVEkXV1z8jr_aN0JbBjHyFh";
+    const downloadUrl = "https://drive.google.com/uc?export=download&id=1n_uUXej_Tsrsf62a9_GX9Rm2gqgv6UK5";
     const downloadLink = document.createElement('a');
     downloadLink.href = downloadUrl;
     downloadLink.target = '_blank'; // This opens in new tab
